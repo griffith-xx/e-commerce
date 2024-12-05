cb7c7046bb6a5c9b01590b590a5169e4f70e0a15
sphinx-p <chacritpoonpipit@gmail.com>
2024-12-05 16:52:29 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,315 @@
+<template>
+  <div>
+    <header class="bg-black text-white py-3.5">
+      <div class="container mx-auto relative">
+        <div class="flex justify-center gap-3">
+          <p>
+            Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+          </p>
+          <a class="font-semibold underline" href="#">Shop Now</a>
+        </div>
+
+        <button
+          class="absolute top-1/2 -translate-y-1/2 right-0 flex gap-1 items-center justify-center"
+          type="button"
+        >
+          <span> English </span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-5 pt-0.5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m19.5 8.25-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </button>
+      </div>
+    </header>
+    <nav class="bg-white text-black border-b border-[#B3B3B3] pt-8 pb-4">
+      <div class="container mx-auto flex justify-between items-center">
+        <a class="font-bold text-2xl" href="#">Exclusive</a>
+        <ul class="flex gap-6">
+          <li class="underline font-medium underline-offset-4 hover:underline">
+            <a href="#">Home</a>
+          </li>
+          <li class="font-medium underline-offset-4 hover:underline">
+            <a href="#">Contact</a>
+          </li>
+          <li class="font-medium underline-offset-4 hover:underline">
+            <a href="#">About</a>
+          </li>
+          <li class="font-medium underline-offset-4 hover:underline">
+            <a href="#">Sign Up</a>
+          </li>
+        </ul>
+        <div class="flex items-center gap-12">
+          <form action="#">
+            <div class="relative">
+              <input
+                class="bg-[#F5F5F5] rounded-md px-3 py-2 w-64"
+                type="text"
+                placeholder="What Are You Looking for"
+              />
+              <div class="absolute top-1/2 -translate-y-1/2 right-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </form>
+          <div class="flex items-center gap-6">
+            <button type="button">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-7"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                />
+              </svg>
+            </button>
+            <button type="button">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-7"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <main>
+      <section class="bg-white text-black">
+        <div class="container mx-auto grid grid-cols-12">
+          <aside class="col-span-3 border-r border-[#B3B3B3] pt-10 px-5 h-fit">
+            <ul class="space-y-1">
+              <li class="py-1.5 hover:underline">
+                <a class="flex justify-between items-center" href="#">
+                  <span class="font-medium"> Woman's Fashion </span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-4 stroke-2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                    />
+                  </svg>
+                </a>
+              </li>
+              <li class="py-1.5 hover:underline">
+                <a class="flex justify-between items-center" href="#">
+                  <span class="font-medium"> Men's Fashion </span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-4 stroke-2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                    />
+                  </svg>
+                </a>
+              </li>
+              <li class="py-1.5 hover:underline">
+                <a class="block font-medium" href="#">
+                  <span> Electronics </span>
+                </a>
+              </li>
+              <li class="py-1.5 hover:underline">
+                <a class="block font-medium" href="#">
+                  <span> Home & Lifestyle </span>
+                </a>
+              </li>
+              <li class="py-1.5 hover:underline">
+                <a class="block font-medium" href="#">
+                  <span> Medicine </span>
+                </a>
+              </li>
+              <li class="py-1.5 hover:underline">
+                <a class="block font-medium" href="#">
+                  <span> Sports & Outdoor </span>
+                </a>
+              </li>
+              <li class="py-1.5 hover:underline">
+                <a class="block font-medium" href="#">
+                  <span>Baby's & Toys </span>
+                </a>
+              </li>
+              <li class="py-1.5 hover:underline">
+                <a class="block font-medium" href="#">
+                  <span>Groceries & Pets </span>
+                </a>
+              </li>
+              <li>
+                <a class="block font-medium" href="#">
+                  <span> Health & Beauty </span>
+                </a>
+              </li>
+            </ul>
+          </aside>
+          <div class="col-span-9 pt-10 px-10">
+            <div
+              class="bg-black h-[345px] text-white w-full px-16 flex justify-between items-center"
+            >
+              <div>
+                <div class="flex items-center gap-7 mb-5">
+                  <img
+                    src="../assets/images/apple_gray_logo.png"
+                    alt="Apple Gray Logo"
+                  />
+                  <span class="mt-2 tracking-widest"> iPhone 14 Series </span>
+                </div>
+                <h1 class="text-5xl font-semibold tracking-wide leading-normal">
+                  Up to 10% <br />
+                  off Voucher
+                </h1>
+                <button class="flex items-center gap-4 mt-5">
+                  <span class="underline underline-offset-8"> Shop Now </span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-5"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <img src="../assets/images/iphone.png" alt="iphone" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="bg-white text-black mt-40">
+        <div class="container mx-auto">
+          <div class="flex items-center gap-4">
+            <div class="bg-[#DB4444] w-5 h-12 rounded-md"></div>
+            <h2 class="text-[#DB4444] font-semibold">To Day's</h2>
+          </div>
+          <div class="flex justify-between mt-8">
+            <div class="flex items-end gap-16">
+              <h2 class="text-4xl font-semibold tracking-wide pb-1">
+                Flash Sales
+              </h2>
+              <div class="flex items-center gap-3">
+                <div class="flex flex-col gap-1">
+                  <span class="font-semibold"> Days </span>
+                  <h3 class="text-5xl font-bold tracking-wider">03</h3>
+                </div>
+                <div class="flex flex-col gap-3 pt-7 px-2">
+                  <div class="bg-[#DB4444] w-1 h-1 rounded-full"></div>
+                  <div class="bg-[#DB4444] w-1 h-1 rounded-full"></div>
+                </div>
+                <div class="flex flex-col gap-1">
+                  <span class="font-semibold"> Hours </span>
+                  <h3 class="text-5xl font-bold tracking-wider">23</h3>
+                </div>
+                <div class="flex flex-col gap-3 pt-7 px-2">
+                  <div class="bg-[#DB4444] w-1 h-1 rounded-full"></div>
+                  <div class="bg-[#DB4444] w-1 h-1 rounded-full"></div>
+                </div>
+                <div class="flex flex-col gap-1">
+                  <span class="font-semibold"> Minutes </span>
+                  <h3 class="text-5xl font-bold tracking-wider">19</h3>
+                </div>
+                <div class="flex flex-col gap-3 pt-7 px-2">
+                  <div class="bg-[#DB4444] w-1 h-1 rounded-full"></div>
+                  <div class="bg-[#DB4444] w-1 h-1 rounded-full"></div>
+                </div>
+                <div class="flex flex-col gap-1">
+                  <span class="font-semibold"> Seconds </span>
+                  <h3 class="text-5xl font-bold tracking-wider">56</h3>
+                </div>
+              </div>
+            </div>
+            <div class="flex items-center gap-3">
+              <button class="bg-[#F5F5F5] p-2 rounded-full" type="button">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+                  />
+                </svg>
+              </button>
+              <button class="bg-[#F5F5F5] p-2 rounded-full" type="button">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div class="mt-10"></div>
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: [
+        '"Inter", sans-serif',
+        {
+          fontFeatureSettings: '"cv11", "ss01"',
+          fontVariationSettings: '"opsz" 32',
+        },
+      ],
+    },
+  },
+  plugins: [],
+};
